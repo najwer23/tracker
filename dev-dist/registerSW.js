a81b909571a941988777dadcc1eed7d084a3e119
Mariusz Najwer <najwer23@gmail.com>
2025-06-08 14:02:04 +0200

    manifest.json

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/tracker/dev-sw.js?dev-sw', { scope: '/tracker/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,44 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tracker/',
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // devOptions: {
+      //   enabled: true,
+      // },
+      manifest: {
+        name: 'Tracker',
+        short_name: 'Tracker',
+        start_url: '/tracker/',
+        scope: '/tracker/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icon/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'icon/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      }
+      
+    }),
+  ],
   build: {
     outDir: './build',
 
