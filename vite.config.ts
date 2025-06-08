53896f05609270b0181e18171c7b9414405f02ed
Mariusz Najwer <najwer23@gmail.com>
2025-06-08 12:30:28 +0200

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/tracker/',
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   build: {
     outDir: './build',
 
