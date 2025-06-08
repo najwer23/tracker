0194a2c95d63a04573a14d4e269d3e3a7b3a5f3d
Mariusz Najwer <najwer23@gmail.com>
2025-06-08 16:14:18 +0200

    Marker

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -2,11 +2,22 @@ import { useEffect, useState } from 'react';
 import styles from './Home.module.css';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 interface Position {
   latitude: number;
   longitude: number;
 }
+
+const customIcon = L.icon({
+  iconUrl: 'marker.svg',   // path to your custom marker image
+  iconSize: [25, 41],                         // size of the icon [width, height]
+  iconAnchor: [12, 41],                       // point of the icon which corresponds to marker's location
+  popupAnchor: [1, -34],                      // point from which the popup should open relative to the iconAnchor
+  shadowSize: [41, 41],                       // size of the shadow
+  shadowAnchor: [12, 41],                     // anchor for the shadow
+});
+
 
 export const Home: React.FC = () => {
   const [position, setPosition] = useState<Position | null>(null);
@@ -53,12 +64,12 @@ export const Home: React.FC = () => {
           </div>
 
           <MapContainer
-            center={[position.latitude, position.longitude]}
-            zoom={13}
+            center={[position.latitude, +position.longitude]}
+            zoom={17}
             scrollWheelZoom={false}
             style={{ height: '800px', width: '100%' }}>
             <TileLayer attribution="" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[position.latitude, position.longitude]}>
+            <Marker position={[position.latitude, position.longitude]} icon={customIcon}>
               <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
               </Popup>
