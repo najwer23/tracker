691a9ef22ba3a9eb768d0ae627ec94501cb0a436
Mariusz Najwer <najwer23@gmail.com>
2025-06-08 15:54:10 +0200

    Leaflet test

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import styles from './Home.module.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 interface Position {
   latitude: number;
@@ -17,7 +19,7 @@ export const Home: React.FC = () => {
     }
 
     const success = (pos: GeolocationPosition) => {
-      console.log(pos.coords.latitude, pos.coords.longitude)
+      console.log(pos.coords.latitude, pos.coords.longitude);
       setPosition({
         latitude: pos.coords.latitude,
         longitude: pos.coords.longitude,
@@ -44,10 +46,25 @@ export const Home: React.FC = () => {
       {error && <p style={{ color: 'red' }}>{error}</p>}
       {!error && !position && <p>Loading position...</p>}
       {position && (
-        <div>
-          <p>Latitude: {position.latitude}</p>
-          <p>Longitude: {position.longitude}</p>
-        </div>
+        <>
+          <div>
+            <p>Latitude: {position.latitude}</p>
+            <p>Longitude: {position.longitude}</p>
+          </div>
+
+          <MapContainer
+            center={[position.latitude, position.longitude]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: '800px', width: '100%' }}>
+            <TileLayer attribution="" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[position.latitude, position.longitude]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </>
       )}
     </div>
   );
