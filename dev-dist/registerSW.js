7d60db60f36f665984ae9b50968c7eff7c7732da
Mariusz Najwer <najwer23@gmail.com>
2025-06-08 15:12:18 +0200

    Test Geo API

--- dev-dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/tracker/dev-sw.js?dev-sw', { scope: '/tracker/', type: 'classic' })
--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,5 +1,54 @@
+import { useEffect, useState } from 'react';
 import styles from './Home.module.css';
 
-export const Home = () => {
-  return <div className={styles.container}>Hello World!</div>;
+interface Position {
+  latitude: number;
+  longitude: number;
+}
+
+export const Home: React.FC = () => {
+  const [position, setPosition] = useState<Position | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError('Geolocation is not supported by your browser');
+      return;
+    }
+
+    const success = (pos: GeolocationPosition) => {
+      console.log(pos.coords.latitude, pos.coords.longitude)
+      setPosition({
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+      });
+      setError(null);
+    };
+
+    const failure = (err: GeolocationPositionError) => {
+      setError(`Error getting position: ${err.message}`);
+    };
+
+    const watcherId = navigator.geolocation.watchPosition(success, failure, {
+      enableHighAccuracy: true,
+      maximumAge: 0,
+      timeout: 10000,
+    });
+
+    return () => navigator.geolocation.clearWatch(watcherId);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1>Hello World!</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {!error && !position && <p>Loading position...</p>}
+      {position && (
+        <div>
+          <p>Latitude: {position.latitude}</p>
+          <p>Longitude: {position.longitude}</p>
+        </div>
+      )}
+    </div>
+  );
 };
