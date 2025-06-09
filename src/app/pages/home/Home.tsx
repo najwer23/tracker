6f16e930844a25e3c2679641244176317ecbc410
Mariusz Najwer <najwer23@gmail.com>
2025-06-09 08:53:15 +0200

    extension

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -178,7 +178,7 @@ export const Home: React.FC = () => {
 
   return (
     <div style={{ height: '100vh', width: '100%', padding: 10 }}>
-      <h1>Traveled Distance and Path 3.0</h1>
+      <h1>Traveled Distance and Path 3.3</h1>
 
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
@@ -221,7 +221,7 @@ export const Home: React.FC = () => {
             <SetInitialView position={startPosition} />
             <TileLayer
               attribution="&copy; OpenStreetMap contributors"
-              url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}"
+              url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
             />
             <Marker position={[position.latitude, position.longitude]} icon={customIcon}>
               <Popup>
